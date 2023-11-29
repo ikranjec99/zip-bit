86f0aa67e9fc73b0858496d882204c233c4dc5be
Ivan Kranjec <76230214+ikranjec99@users.noreply.github.com>
2023-11-30 00:20:24 +0100

    [DEV] Fix logging - log to console (#2)
    * [DEV] Fix logging - use serilog and read config from appsettings...
    
    * [DEV] Add logging to handler.
    
    * [DEV] Update some extensions...

---
+++ ZipBit/ZipBit.API/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace ZipBit.API.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        public static void Configure(this IHostBuilder builder) =>
+            builder.UseSerilog((hostingContext, loggerConfiguration) => 
+            { 
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration); 
+            });
+    }
+}
