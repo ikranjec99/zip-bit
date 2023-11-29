8250ef46ec7fc1e82788478bde0dd0521c65e3b4
Ivan <ivan.kranjec95@gmail.com>
2023-11-29 22:33:22 +0100

    [DEV] Add db scripts...

---
+++ ZipBit/ZipBit.Core/DataAccess/ZipBitDb/Sql/InsertUrl.sql
@@ -0,0 +1,7 @@
+﻿--DECLARE @UrlOriginal		VARCHAR(2048) = 'https://www.google.hr'
+--DECLARE @UrlShortened	    VARCHAR(2048) = 'https://zipbit.io/cbedh419'
+
+INSERT INTO [dbo].[Url] ([UrlOriginal], [UrlShortened])
+VALUES (@UrlOriginal, @UrlShortened);
+
+SELECT SCOPE_IDENTITY();
