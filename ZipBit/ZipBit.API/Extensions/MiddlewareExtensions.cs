ca1bbd5c2956df10fe52c2d092755c97b40e9382
Ivan <ivan.kranjec95@gmail.com>
2023-11-29 22:31:19 +0100

    [DEV] Add minimum middleware.

---
+++ ZipBit/ZipBit.API/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ZipBit.API.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static void ConfigureMiddlewares(this WebApplication app)
+        {
+            app.UseDefaultFiles()
+            .UseStaticFiles()
+            .UseSwagger()
+            .UseSwaggerUI()
+            .UseHttpsRedirection()
+            .UseAuthorization();
+        }
+    }
+}
