7221df13b8768279283c9ec9ab31dfd5db32d63d
Ivan <ivan.kranjec95@gmail.com>
2023-11-29 22:34:10 +0100

    [DEV] Add basic logging...

---
+++ ZipBit/ZipBit.API/Helpers/LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using Serilog;
+using ZipBit.API.Extensions;
+
+namespace ZipBit.API.Helpers
+{
+    public static class LogHelper
+    {
+        public static void EnrichFromRequest(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            diagnosticContext.Set(nameof(httpContext.Connection.LocalIpAddress), httpContext.Connection.LocalIpAddress);
+            diagnosticContext.Set(nameof(httpContext.Connection.RemoteIpAddress), httpContext.Connection.RemoteIpAddress);
+
+            var request = httpContext.Request;
+
+            diagnosticContext.Set(nameof(request.Host), request.Host);
+            diagnosticContext.Set("RequestMethod", request.Method);
+            diagnosticContext.Set("Resource", request.Path.Value);
+            diagnosticContext.Set(nameof(request.Scheme), request.Scheme);
+            diagnosticContext.Set(nameof(httpContext.Response.ContentType), httpContext.Response.ContentType);
+
+            foreach (string headerKey in request.Headers.Keys.Where(key => !key.Contains("Cookie")
+                                                                           && !key.Contains("credentials")
+                                                                           && !key.Contains("Authorization")))
+
+                diagnosticContext.Set(headerKey, request.Headers.GetValue(headerKey));
+
+            foreach (var route in request.RouteValues) diagnosticContext.Set(route.Key, route.Value);
+
+        }
+    }
+}
---
+++ ZipBit/ZipBit.Core/Extensions/ILoggerExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ZipBit.Core.Extensions
+{
+    public static class ILoggerExtensions
+    {
+        public static void LogError(this ILogger logger, string methodName, Exception e)
+            => logger.LogError(e, "{MethodName} failed: {Message}", methodName, e.Message);
+    }
+}
