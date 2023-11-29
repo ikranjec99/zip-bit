6b74926291bdfa5811ce39ddca8e16ea0807229c
Ivan <ivan.kranjec95@gmail.com>
2023-11-29 22:33:47 +0100

    [DEV] Add db script loader...

---
+++ ZipBit/ZipBit.Core/DataAccess/ZipBitDb/Sql/ZipBitDbLoader.cs
@@ -0,0 +1,13 @@
+﻿using ZipBit.Core.Extensions;
+
+namespace ZipBit.Core.DataAccess.ZipBitDb.Sql
+{
+    public static class ZipBitDbLoader
+    {
+        public static string Load(string scriptName)
+        {
+            string resourceName = $"{nameof(ZipBit)}.{nameof(Core)}.{nameof(DataAccess)}.{nameof(ZipBitDb)}.{nameof(Sql)}.{scriptName}.sql";
+            return typeof(ZipBitDbLoader).Assembly.ReadResourceText(resourceName).Insert(0, $"--{scriptName}\n");
+        }
+    }
+}
