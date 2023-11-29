47a32cedf04887a63e5fdf8557211975d532847a
Ivan <ivan.kranjec95@gmail.com>
2023-11-30 00:17:03 +0100

    [DEV] Update some extensions...

--- ZipBit/ZipBit.Core/Extensions/ZipBitEventExtensions.cs
+++ ZipBit/ZipBit.Core/Extensions/ZipBitEventExtensions.cs
@@ -1,7 +1,10 @@
-﻿namespace ZipBit.Core.Extensions
+﻿using Microsoft.Extensions.Logging;
+using ZipBit.Core.Constants;
+
+namespace ZipBit.Core.Extensions
 {
     public static class ZipBitEventExtensions
     {
-        public static EventId ToEventId(this ZipBitEvent event) => new EventId((int) event, event.ToString());
+        public static EventId ToEventId(this ZipBitEvent zipBitEvent) => new EventId((int)zipBitEvent, zipBitEvent.ToString());
     }
 }
