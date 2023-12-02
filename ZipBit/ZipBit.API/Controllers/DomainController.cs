e4880013b9f1fbac510cca5041232f0c0cc3bf2a
Ivan <ivan.kranjec95@gmail.com>
2023-12-02 02:49:25 +0100

    [DEV] Add DomainController...

---
+++ ZipBit/ZipBit.API/Controllers/DomainController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZipBit.Core.Business.Interfaces;
+using ZipBit.Core.Business.Models;
+
+namespace ZipBit.API.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/zipbit/{version:apiVersion}/domain")]
+    public sealed class DomainController : ControllerBase
+    {
+        private readonly IDomainHandler _domainHandler;
+
+        public DomainController(IDomainHandler domainHandler)
+        {
+            _domainHandler = domainHandler;
+        }
+
+        /// <summary>
+        /// Add new domain
+        /// </summary>
+        /// <returns>Create new domain for separate shortening service</returns>
+        [HttpPost]
+        public async Task<CreateDomainRequestResponse> PostDomain([FromBody] CreateDomainRequest request)
+            => await _domainHandler.CreateDomain(request);
+    }
+}
