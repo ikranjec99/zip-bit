26ccdff2296534f2c5df6eeeeb0b901929788582
Ivan <ivan.kranjec95@gmail.com>
2023-12-02 02:47:32 +0100

    [DEV] Add models and excepitons...

---
+++ ZipBit/ZipBit.Core/Business/Exceptions/DomainAlreadyExistsException.cs
@@ -0,0 +1,7 @@
+﻿namespace ZipBit.Core.Business.Exceptions
+{
+    public class DomainAlreadyExistsException : BusinessException
+    {
+        public DomainAlreadyExistsException(string name) : base($"Domain with name {name} already exists.") { }
+    }
+}
