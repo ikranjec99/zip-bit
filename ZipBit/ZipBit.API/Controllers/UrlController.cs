800ce65a5c8e05c8ff433d0c4d2dc54593f5c490
Ivan <ivan.kranjec95@gmail.com>
2023-12-09 23:06:55 +0100

    [DEV] Add redirect route to Url controller.

--- ZipBit/ZipBit.API/Controllers/UrlController.cs
+++ ZipBit/ZipBit.API/Controllers/UrlController.cs
@@ -21,7 +21,18 @@ namespace ZipBit.API.Controllers
         /// </summary>
         /// <returns>Returns URL shortening result.</returns>
         [HttpPost]
-        public async Task<CreateShortenedUrlResponse> PostUrl([FromBody] CreateShortenedUrlRequest request) =>
-            await _urlHandler.CreateShortenedUrl(request);
+        public async Task<CreateShortenedUrlResponse> PostUrl([FromBody] CreateShortenedUrlRequest request) 
+            => await _urlHandler.CreateShortenedUrl(request);
+
+        /// <summary>
+        /// Redirect to url behind short code
+        /// </summary>
+        /// <returns>Returns 302 and redirects to url behind code if url exists</returns>
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetRedirectUrl([FromRoute] string code)
+        {
+            var url = await _urlHandler.GetUrlByCode(new GetUrlByCodeRequest { Code = code });
+            return Redirect(url.Url);
+        }
     }
 }
